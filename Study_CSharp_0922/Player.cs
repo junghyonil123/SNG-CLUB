287f47b485865692ef5c5eb118b04c3aadc90bc0
junghyonil123 <90584642+junghyonil123@users.noreply.github.com>
2022-09-24 19:25:48 +0900

    2022_09_24

---
+++ Study_CSharp_0922/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace test0922
+{
+    public class Player
+    {
+        public int hp = 100;
+        public int at = 10;
+
+  
+        public void AddAt(int _at)
+        {
+            //공격력을 _at올려주기 위한 함수
+            at += _at;
+        }
+
+
+    }
+}
---
+++ Study_CSharp_0922/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test0922
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            
+            
+            Monster monster1 = new Monster();
+
+            player.AddAt(10);
+
+            Console.WriteLine(monster1.at);
+            Console.WriteLine(player.at);
+            Console.ReadLine();
+
+            
+        }
+    }
+
+}
